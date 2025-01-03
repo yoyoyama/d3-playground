7e1c6396fd45f4afa38d05d0c2ae507bb804c5c7
yoyoyama <mt.yoyoyama@gmail.com>
2025-01-03 23:54:14 +0900

    Arc

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,39 @@
+import { useCallback, useEffect, useState } from 'react';
+import { Arc } from './components/Arc';
+import type { Data as ArcData } from './components/Arc';
+
+import styles from './App.module.css';
+
 export default function App() {
-  return <></>;
+  const [arcData, setArcData] = useState<ArcData>(0);
+
+  const generateArcData = useCallback(() => {
+    const data = Math.random();
+    setArcData(data);
+  }, []);
+
+  const generateData = useCallback(() => {
+    generateArcData();
+  }, [generateArcData]);
+
+  useEffect(() => {
+    generateData();
+  }, [generateData]);
+
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>d3-playground</h1>
+        <button type="button" onClick={generateData}>
+          Regenerate Data
+        </button>
+      </header>
+      <main className={styles.main}>
+        <section>
+          <h2>Arc</h2>
+          <Arc data={arcData} />
+        </section>
+      </main>
+    </>
+  );
 }
---
+++ src/components/Arc/index.tsx
@@ -0,0 +1,44 @@
+import { ComponentPropsWithoutRef, useMemo } from 'react';
+import * as d3 from 'd3';
+
+import styles from './Arc.module.css';
+
+export type Data = number;
+
+type Props = ComponentPropsWithoutRef<'svg'> & {
+  data: Data;
+  size?: number;
+};
+
+export function Arc({ data, size = 240, ...props }: Props) {
+  const label = useMemo(() => {
+    return (data * 100).toLocaleString('ja-JP', { maximumFractionDigits: 1 });
+  }, [data]);
+
+  const pathData = useMemo(() => {
+    const outerRadius = size / 2;
+    const innerRadius = outerRadius * 0.75;
+    const arc = d3.arc().cornerRadius(16);
+    const tau = 2 * Math.PI;
+
+    const back = arc({ innerRadius, outerRadius, startAngle: 0, endAngle: tau }) ?? '';
+
+    const front = arc({ innerRadius, outerRadius, startAngle: 0, endAngle: data * tau }) ?? '';
+
+    return { back, front };
+  }, [data, size]);
+
+  return (
+    <div className={styles.arc}>
+      <svg width={size} height={size} {...props}>
+        <g transform={`translate(${size / 2}, ${size / 2})`}>
+          <path d={pathData.back} className={styles.backPath} />
+          <path d={pathData.front} className={styles.frontPath} />
+        </g>
+      </svg>
+      <p className={styles.label}>
+        <span className={styles.value}>{label}</span>%
+      </p>
+    </div>
+  );
+}
